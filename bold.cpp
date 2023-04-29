2a23f0992bf0acc133af3fcb910bb58ef951b714
Peeranat Sugsavanvit <118276975+CapClark@users.noreply.github.com>
2023-04-29 07:38:30 +0700

    Add files via upload

---
+++ bold.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+#include <algorithm>
+#include <string>
+#include <cstring>
+#include <set>
+
+using namespace std;
+int main()
+{
+    int start = 0;
+    string str;
+    getline(cin, str);
+    vector<string> words;
+    for (int i = 0; i < str.length(); i++)
+    {
+        if (str[i] == ' ' || i == str.size() - 1)
+        {
+            int wordLength = (i == str.size() - 1) ? i - start + 1 : i - start;
+            string word = str.substr(start, wordLength);
+            words.push_back(word);
+            start = i + 1; // update the start position for the next word
+        }
+    }
+    vector<float> size_words;
+    for (int i = 0; i < words.size(); i++)
+    {
+        size_words.push_back(floor(words[i].size() / 2));
+    }
+    for (int k = 0; k < words.size(); k++)
+    {
+        cout << "\033[1m" << words[k].substr(0, size_words[k]) << "\033[0m" << words[k].substr(size_words[k]) << " ";
+    }
+    return 0;
+}
